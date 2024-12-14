6c9de249598512f01147e110d6c86971d584f38f
Dalton Dam <daltondam26@gmail.com>
2024-12-14 15:24:23 -0300

    refact: Refatoramento de código, criacção de alguns endpoints e problema com o Post resolvido

--- src/main/java/com/cinevault/service/MovieService.java
+++ src/main/java/com/cinevault/service/MovieService.java
@@ -1,24 +1,48 @@
 package com.cinevault.service;
 
+import com.cinevault.entity.Category;
 import com.cinevault.entity.Movie;
+import com.cinevault.entity.Streaming;
 import com.cinevault.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(Long  id) {
+        return movieRepository.findById(id);
+    }
+
+    private List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> categoryService.findById(category.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
     }
 
 }
