3e2388972bf6bbd64f7e664d4b39199743fc6257
Dalton Dam <daltondam26@gmail.com>
2024-12-16 11:20:25 -0300

    feat: finalizado todos os endpoints

--- src/main/java/com/cinevault/repository/MovieRepository.java
+++ src/main/java/com/cinevault/repository/MovieRepository.java
@@ -1,9 +1,16 @@
 package com.cinevault.repository;
 
+import com.cinevault.entity.Category;
 import com.cinevault.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategories(List<Category> categories);
+
+    List<Movie> findTop5ByOrderByRatingDesc();
 }
